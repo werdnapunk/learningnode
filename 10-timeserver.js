dc28e3c8519035beac332927e0abbe070be3ca4a
werdnapunk <ajduff5@gmail.com>
2013-10-30 18:54:15 -0500

    Finished the timeserver!

--- 10-timeserver.js
+++ 10-timeserver.js
@@ -2,8 +2,8 @@ var net = require('net')
 var moment = require('moment')
 
 var server = net.createServer(function(socket){
-	data = moment().format('YYYY[-]MM[-]DD hh:mm')
-	data.toString()
-	socket.end(data)
+        data = moment().format('YYYY[-]MM[-]DD HH:mm')
+        data.toString()
+        socket.end(data + '\n')
 })
 server.listen(8000)
